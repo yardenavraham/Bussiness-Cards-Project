43e14358d01f36b5a32a789b7e8afb16831f03ef
your <yarden8bi@gmail.com>
2023-07-17 21:22:37 +0300

    pdf download

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,12 @@ function App() {
     'site': ''
   });
   const [items, setItems] = useState([])
+  const [isDownload, setIsDownload] = useState(false);
+
+  useEffect(() => {
+
+  }, [isDownload])
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     const result = await createItem(item);
@@ -52,12 +58,30 @@ function App() {
     }
   }
 
+
+
   const createItem = async (item) => {
     try {
       //Download PDF card
-      const card = new jsPDF('portrait', 'pt', 'a4');
-      card.html(document.querySelector('#pdfCard')).then(() => {
-        card.save('bussiness-card.pdf');
+
+      var doc = new jsPDF()
+
+      var startY = 10; // Starting Y position for text and image
+
+      // Add text to the PDF
+      doc.text(item.name + '\n' + item.phone + '\n' + item.type + '\n' + item.email + '\n' + item.site, 10, startY);
+
+      // Load the image
+      var img = new Image();
+      img.src = item.image;
+
+      // Wait for the image to load
+      img.addEventListener('load', function () {
+        // Add the image to the PDF
+        doc.addImage(img, 'PNG', 10, startY + 30, 50, 50); // Adjust the coordinates and size as per your requirements
+
+        // Save the PDF
+        doc.save('bussiness-card.pdf');
       });
 
       //Save to mongoDB
@@ -87,40 +111,40 @@ function App() {
             <TextField onChange={e => setItem({ ...item, type: e.target.value })} id="outlined-basic" label="Type" variant="outlined" />
             <TextField onChange={e => setItem({ ...item, site: e.target.value })} id="outlined-basic" label="Site" variant="outlined" />
             <TextField onChange={e => setItem({ ...item, email: e.target.value })} id="outlined-basic" label="Email" variant="outlined" />
-
             <FileBase64
               type="file"
               multiple={false}
               onDone={({ base64 }) => setItem({ ...item, image: base64 })}
             /></div>
 
-
         </form>
 
         <div style={{
           display: "flex",
           flexDirection: "column",
-              alignItems: "center"}}>
-        {items?.map(item => (
+          alignItems: "center"
+        }}>
+          {items?.map(item => (
 
-          <div id="pdfCard" className="card" key={item._id} style={{
-            border: '3px solid #1976d2', margin: '5px',
-            width: "20rem"
+            <div id="pdfCard" className="card" key={item._id} style={{
+              border: '3px solid #1976d2', margin: '5px',
+              width: "20rem"
 
-          }}>
-            <p>{item.name}</p>
-            <p>{item.phone}</p>
-            <p>{item.type}</p>
-            <p>{item.email}</p>
-            <p>{item.site}</p>
-            <img className="activator" style={{
-              objectFit: 'cover',
-              width: '100px',
-              height: '100px'
-            }} src={item.image} />
-          </div>
-        ))}</div>
-    </div></div >
+            }}>
+              <p>{item.name}</p>
+              <p>{item.phone}</p>
+              <p>{item.type}</p>
+              <p>{item.email}</p>
+              <p>{item.site}</p>
+              <img className="activator" style={{
+                objectFit: 'cover',
+                width: '100px',
+                height: '100px'
+              }} src={item.image} />
+            </div>
+          ))}
+        </div>
+      </div></div >
   );
 }
 
